6afaaacf35a1760fb72e68a204b17f33c5907444
lijian <4130520@qq.com>
2021-08-30 10:38:29 +0800

    调整认证失败时的message

--- auth-spring-boot-starter/src/main/java/cc/lj1/auth/exception/AuthFailedException.java
+++ auth-spring-boot-starter/src/main/java/cc/lj1/auth/exception/AuthFailedException.java
@@ -1,4 +1,11 @@
 package cc.lj1.auth.exception;
 
 public class AuthFailedException extends Exception {
+    public AuthFailedException() {
+        super("需要登录");
+    }
+
+    public AuthFailedException(String message) {
+        super(message);
+    }
 }
--- auth-spring-boot-starter/src/main/java/cc/lj1/auth/exception/AuthForbiddenException.java
+++ auth-spring-boot-starter/src/main/java/cc/lj1/auth/exception/AuthForbiddenException.java
@@ -1,4 +1,11 @@
 package cc.lj1.auth.exception;
 
 public class AuthForbiddenException extends Exception {
+    public AuthForbiddenException() {
+        super("没有权限");
+    }
+
+    public AuthForbiddenException(String message) {
+        super(message);
+    }
 }
